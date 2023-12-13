36146075f681097be215ac0b59fc54e4ae415bc7
Qudratullah Ramzi <138484806+qudratramzi@users.noreply.github.com>
2023-12-13 11:51:31 +0100

    Create new test.py
    data type

---
+++ gitpytest for bigenner/new test.py
@@ -0,0 +1,50 @@
+print('hello world')
+print('hello world')
+x= 1
+y=2.4
+z= '5'
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x=int(1)
+y= int(2.4)
+z= int('5')
+
+print(x)
+print(y)
+print(z)
+
+x= float(1)  
+y= float(2.5)
+z=float('56')
+
+
+print(type(x))  # hte answer will be float
+print(type(y)) # the answer will be float
+print(type(z))  # the answer will be float
+
+x= str(1)
+y= str(4.6)
+z= str('66')
+
+print(type(x)) # the answer will be str
+print(type(y))  # the answer will be str
+print(type(z))  # the answer will be str
+
+b= 'welcone to the python'
+print(b[2:6])
+
+a= 'ali wilcome to dubai'
+print(a[:6])
+print(a[1:5])
+print(a[2:6])
+print(a[:-2])
+print(a.upper())
+print(a.upper())
+a= 'ALI WILCONE TO KABUL'
+print(a.lower())
+a= '   ali wilcome to dubai'
+print(a.strip())
