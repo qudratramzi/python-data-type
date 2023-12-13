ce63c0e8e2f88201b78bf3811bf9e54898c464de
Qudratullah Ramzi <138484806+qudratramzi@users.noreply.github.com>
2023-12-13 12:27:24 +0100

    Create string.py
    string python

---
+++ gitpytest for bigenner/string.py
@@ -0,0 +1,12 @@
+# string in python sorounded by double cotesion or single quotation and both of them are the same 
+# for example "hello" is the same with 'hello'
+print('hello')
+print("hello")
+print('hello guys all over the world')
+a= 'hello my younger brother'
+print(a.upper())
+b= '   I REALLY DO NOT KNOW WHAT DO YOU MEAN'
+#print(b.lower())
+#print(b.strip())
+print(b.split())
+print(b[2:9])
